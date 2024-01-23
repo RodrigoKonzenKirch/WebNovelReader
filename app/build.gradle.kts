c68710dd08a1a87fef11d021513bcea7342e44cf
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2024-01-22 23:28:22 -0300

    Set Up Hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -40,7 +43,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.5"
     }
     packaging {
         resources {
@@ -50,15 +53,29 @@ android {
 }
 
 dependencies {
+    val hilt_version = "2.48.1"
+    val room_version = "2.6.1"
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.dagger:hilt-android:$hilt_version")
+    ksp("com.google.dagger:hilt-compiler:$hilt_version")
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/webnovelreader/MainActivity.kt
+++ app/src/main/java/com/example/webnovelreader/MainActivity.kt
@@ -1,28 +1,17 @@
 package com.example.webnovelreader
 
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.view.ViewGroup
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.viewinterop.AndroidView
+import com.example.webnovelreader.compose.MainScreen
 import com.example.webnovelreader.ui.theme.WebNovelReaderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,55 +29,4 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    CustomWebView(modifier, "https://www.uukanshu.net/b/33939/109401.html#gsc.tab=0")
-}
 
-@Composable
-fun CustomWebView(
-    modifier: Modifier,
-    url: String,
-) {
-
-    var backEnabled by remember { mutableStateOf(false) }
-    var webView: WebView? = null
-    var title = rememberSaveable {
-        mutableStateOf("")
-    }
-    Column {
-        Text(text = " Title = " + title.value)
-
-        AndroidView(
-            modifier = modifier,
-            factory = { context ->
-                WebView(context).apply {
-                    layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                    )
-                    webViewClient = object : WebViewClient() {
-                        override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
-                            backEnabled = view.canGoBack()
-                        }
-
-                        override fun onPageFinished(view: WebView?, url: String?) {
-                            title.value = view?.title?:""
-                        }
-                    }
-                    settings.javaScriptEnabled = false
-
-                    loadUrl(url)
-                    webView = this
-                }
-            }, update = {
-                webView = it
-            })
-    }
-
-    BackHandler(enabled = backEnabled) {
-        webView?.goBack()
-    }
-
-
-}
