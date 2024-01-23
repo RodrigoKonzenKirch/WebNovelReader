b006ea83050f8eb26cc4ee15e85288fb7d633312
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2024-01-23 00:00:07 -0300

    Add dependencies for truth, Mockk and coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,8 +74,6 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
-
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -83,4 +81,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    testImplementation("com.google.truth:truth:1.1.4")
+    testImplementation("io.mockk:mockk:1.13.8")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+
 }
