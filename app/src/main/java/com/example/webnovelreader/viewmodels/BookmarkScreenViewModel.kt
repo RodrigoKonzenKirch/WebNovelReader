bbdbfd57a1c56926b51dc32631cbac1cd2e6867d
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2024-02-08 16:52:56 -0300

    Add injection of dispatcher in BookmarkScreenViewModel

--- app/src/main/java/com/example/webnovelreader/viewmodels/BookmarkScreenViewModel.kt
+++ app/src/main/java/com/example/webnovelreader/viewmodels/BookmarkScreenViewModel.kt
@@ -3,8 +3,10 @@ package com.example.webnovelreader.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.webnovelreader.data.Bookmark
+import com.example.webnovelreader.di.DispatcherIo
 import com.example.webnovelreader.domain.BookmarkRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
@@ -14,6 +16,7 @@ import javax.inject.Inject
 @HiltViewModel
 class BookmarkScreenViewModel @Inject constructor(
     private val repository: BookmarkRepository,
+    @DispatcherIo private val dispatcherIo: CoroutineDispatcher
 ): ViewModel() {
 
     private var _allBookmarksList: StateFlow<List<Bookmark>> = repository.allBookmarks()
@@ -25,7 +28,7 @@ class BookmarkScreenViewModel @Inject constructor(
     val allBookmarksList = _allBookmarksList
 
     fun deleteBookmark(bookmark: Bookmark) {
-        viewModelScope.launch {
+        viewModelScope.launch(dispatcherIo) {
             repository.deleteBookmark(bookmark)
         }
     }
