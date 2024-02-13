3598d0621754d0fff93f98ae1b36f563580d23c9
Rodrigo Konzen Kirch <rodrigokirch@gmail.com>
2024-02-13 20:19:10 -0300

    Change dataStore into a singleton

--- app/src/main/java/com/example/webnovelreader/di/DataStoreModule.kt
+++ app/src/main/java/com/example/webnovelreader/di/DataStoreModule.kt
@@ -12,11 +12,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
 object DataStoreModule {
 
+    @Singleton
     @Provides
     fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences>{
         return PreferenceDataStoreFactory.create(
